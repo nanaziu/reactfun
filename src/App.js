e5b393a42f842998a857347925989e1730d7b631
Zuzia <zuzannabrzeczek71@gmail.com>
2023-06-14 00:44:47 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, createContext, useContext } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>(c) nanaziu' terminal. All rights reserved</h2>
+      <br></br>
+      <p>help&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;see all commands</p>
+      <p>clr&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;clear the terminal</p>
+      {/*<p>1  Who are you?</p>
+      <p>2  What's your occupation?</p>
+      <p>3  How old r u?</p>
+      <p>4  Hobbies</p>
+      <p>#  ?</p>
+      <p>5  Education</p>
+      <p>6  Projects</p>*/}
+      <br></br>
+      C:\Users\nanaziu&gt;<input 
+        type="text"
+        value={input}
+        onChange={x=>setInput(x.target.value)}
+        onKeyDown={x=>{
+          if(x.key === "Enter"){
+            if(input === "clr"){
+              setOutput("")
+            }
+            else {
+              setOutput("There's no such command")
+            }
+          }
+        }}
+      />
+            <div className='terminal'>
+        <p>{output}</p>
+      </div>
     </div>
   );
 }
